b4aaeaaa5170b14e722c8c0aa7362c1fe25e4324
Mario <marioclange@gmail.com>
2021-04-13 19:23:00 -0400

    review updating

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv/config");
 require("./db");
 const express = require("express");
 const hbs = require("hbs");
+const Review = require("./models/Review.model");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 
 //Middleware Config
@@ -10,6 +13,10 @@ require("./config/session.config")(app);
 
 //Default title
 app.locals.title = `Welcome to the Animation Station!`;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
 
 //Route Handling
 
@@ -21,6 +28,69 @@ app.use("/", auth);
 
 const dev = require("./routes/dev");
 app.use("/dev/", dev);
+
+app.get('/reviews', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {title: "New Review"});
+});
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
+});
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+});
+
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/reviews');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
 
 //Error Handling
 require("./error-handling")(app);
