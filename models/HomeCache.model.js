6e750be041597d47b134d450e989a64ba2144e63
Martin Stoyanov <martinestoyanov@gmail.com>
2021-04-24 10:41:51 -0400

    adding timestamps to cache model

--- models/HomeCache.model.js
+++ models/HomeCache.model.js
@@ -1,11 +1,16 @@
 const { Schema, model } = require("mongoose");
 
-const homeCacheSchema = new Schema({
-  etag: String,
-  genre: String,
-  genreCode: Number,
-  results: [],
-});
+const homeCacheSchema = new Schema(
+  {
+    etag: String,
+    genre: String,
+    genreCode: Number,
+    results: [],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const HomeCache = model("HomeCache", homeCacheSchema);
 module.exports = HomeCache;
