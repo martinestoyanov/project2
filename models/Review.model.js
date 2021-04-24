e46ed7c2558f552943dd8dce0d52622221ad919d
Martin Stoyanov <martinestoyanov@gmail.com>
2021-04-24 10:19:13 -0400

    reviews in anime view tweaks

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,36 +1,37 @@
 const { Schema, model } = require("mongoose");
 
-const reviewSchema = new Schema({
-  
-  title: String,
-  
-  description: String,
-  
-  animeTitle: String,
-  
-  score: Number,
-  
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+const reviewSchema = new Schema(
+  {
+    title: String,
 
-  // get rid of me
-  // comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  
-  
-  //   user: {
-  //     // type: ObjectId,
-  //     ref: 'User',
-  //     required: [true, 'Review must have an author']
-  //   },
-  //   anime: {
-  //     // type: ObjectId,
-  //     ref: 'Anime',
-  //     required: [true, 'Review must belong to a anime']
-  //   }
-},
+    description: String,
 
-{
-  timestamps: true,
-}
+    animeTitle: String,
+
+    score: Number,
+
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+
+    mal_id: Number,
+
+    // get rid of me
+    // comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+
+    //   user: {
+    //     // type: ObjectId,
+    //     ref: 'User',
+    //     required: [true, 'Review must have an author']
+    //   },
+    //   anime: {
+    //     // type: ObjectId,
+    //     ref: 'Anime',
+    //     required: [true, 'Review must belong to a anime']
+    //   }
+  },
+
+  {
+    timestamps: true,
+  }
 );
 
 const Review = model("Review", reviewSchema);
